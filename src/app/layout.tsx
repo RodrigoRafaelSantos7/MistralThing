03ae8cc60800b6901409c671b47b5cee23cd1464
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-11-14 14:05:03 +0100

    Enhance project configuration and add new dependencies</message>
    <message>Updated `package.json` and `bun.lock` to include new dependencies: `class-variance-authority`, `clsx`, `lucide-react`, `tailwind-merge`, and `tw-animate-css` for improved styling and utility functions. Created a new `components.json` file to define component configurations and updated `globals.css` to incorporate new styles and variables. Modified `layout.tsx` to enhance metadata for better SEO and user engagement. Added a utility function in `utils.ts` for class name management using `clsx` and `tailwind-merge`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,52 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Mistral Thing",
+  title: {
+    default: "Mistral Thing",
+    template: "%s | Mistral Thing",
+  },
+  metadataBase: new URL("https://mistral-thing.xyz"),
   description:
-    "Mistral Thing is a chatbot that uses the Mistral AI API to answer questions and help you with your tasks.",
+    "Mistral Thing is a sleek and modern AI chat application. It allows you to interact with large language models from Mistral",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: [
+    "Mistral Thing",
+    "AI Chat",
+    "AI Chat Application",
+    "AI Chat Application with Mistral AI",
+  ],
+  authors: [
+    {
+      name: "Rodrigo Santos",
+      url: "https://rodrigosantos.dev",
+    },
+  ],
+  creator: "@rrcssantosdev",
+  openGraph: {
+    type: "website",
+    locale: "en",
+    url: "https://mistral-thing.xyz",
+    title: "Mistral Thing",
+    description:
+      "Mistral Thing is a sleek and modern AI chat application. It allows you to interact with large language models from Mistral",
+    siteName: "Mistral Thing",
+    images: ["https://mistral-thing.xyz/og"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mistral Thing",
+    description:
+      "Mistral Thing is a sleek and modern AI chat application. It allows you to interact with large language models from Mistral",
+    images: ["https://mistral-thing.xyz/og"],
+    creator: "@rrcssantosdev",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-video-preview": -1,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+  },
 };
 
 export default function RootLayout({
